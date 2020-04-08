179b003a9471553ee0ca78bb87c4d9d383a50497
João Marcos <gipidesenvolvimento@gmail.com>
2020-04-08 10:52:54 -0300

    OrderItem, many-to-many association with extra attributes - Part 1

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -9,7 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
